694da15689081acd8219b91880c06f889286316e
Anton <reopik@mail.ru>
2023-07-19 23:00:38 +0300

    created header

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import './header.scss';
+
+const Header = () => {
+    return (
+        <header className="weather-header">
+            <h1 className="weather-header__heading">Weather App</h1>
+        </header>
+    );
+}
+
+export default Header;
