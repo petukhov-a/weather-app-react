ae604f861f1fc7cd1edde1541a321889c844d7ab
Anton <reopik@mail.ru>
2023-07-29 23:35:26 +0300

    made fetching visibility, humidity, windspeed, feels like

--- src/components/currentWeather/CurrentWeather.js
+++ src/components/currentWeather/CurrentWeather.js
@@ -26,19 +26,19 @@ const CurrentWeather = (props) => {
         <div className="current">
             <div className="current-weather">
                 <div className="current-weather-center">
-                    <p className="current-weather-center__last-updated"><span>Last updated</span><br/>12-08-2023</p>
+                    <p className="current-weather-center__last-updated"><span>Last updated</span><br/>{currentWeather.last_updated}</p>
                     <p className="current-weather-center__location">{location}</p>
                     <img className="current-weather-center__icon-img"/>
                     <p className="current-weather-center__temperature">{currentWeather.temp_c}°C</p>
                 </div>
                 <div className="current-weather-left">
-                    <p className="current-weather-left__feels"><span>Feels like</span>100</p>
-                    <p className="current-weather-left__rain-chance"><span>Rain chance</span>100%</p>
+                    <p className="current-weather-left__feels"><span>Feels like</span>{currentWeather.feelslike_c}°C</p>
+                    <p className="current-weather-left__rain-chance"><span>Humidity</span>{currentWeather.humidity}%</p>
 
                 </div>
                 <div className="current-weather-right">
-                    <p className="current-weather-right__wind-speed"><span>Wind speed</span>15 км/ч</p>
-                    <p className="current-weather-right__visibility-km"><span>Visibility</span>10км</p>
+                    <p className="current-weather-right__wind-speed"><span>Wind speed</span>{currentWeather.wind_kph} km/h</p>
+                    <p className="current-weather-right__visibility-km"><span>Visibility</span>{currentWeather.vis_km} km</p>
                 </div>
             </div>
         </div>
