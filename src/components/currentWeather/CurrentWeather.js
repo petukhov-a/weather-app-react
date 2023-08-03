d599c8d6016d978b9f81d06eb4992bb45b3d629d
Anton <reopik@mail.ru>
2023-08-03 23:56:27 +0300

    extra current weather info on click on location with animation

--- src/components/currentWeather/CurrentWeather.js
+++ src/components/currentWeather/CurrentWeather.js
@@ -5,6 +5,7 @@ import WeatherService from "../../services/WeatherService";
 const CurrentWeather = ({location}) => {
     const weatherService = new WeatherService();
     const [currentWeather, setCurrentWeather] = useState({});
+    const [active, setActive] = useState(false);
 
     useEffect(() => {
         onRequest();
@@ -19,21 +20,27 @@ const CurrentWeather = ({location}) => {
             .then(onCurrentWeatherLoaded);
     }
 
+    const onLocationClick = () => {
+        setActive(prev => !prev);
+    }
+
+    const clazz = active ? ' active' : '';
+
     return (
         <div className="current">
             <div className="current-weather">
                 <div className="current-weather-center">
                     <p className="current-weather-center__last-updated"><span>Last updated</span><br/>{currentWeather.last_updated}</p>
-                    <p className="current-weather-center__location">{location}</p>
+                    <button onClick={onLocationClick} className="current-weather-center__location">{location}</button>
                     <img className="current-weather-center__icon-img"/>
                     <p className="current-weather-center__temperature">{currentWeather.temp_c}°C</p>
                 </div>
-                <div className="current-weather-left">
+                <div className={"current-weather-left current-weather-side" + clazz}>
                     <p className="current-weather-left__feels"><span>Feels like</span>{currentWeather.feelslike_c}°C</p>
                     <p className="current-weather-left__rain-chance"><span>Humidity</span>{currentWeather.humidity}%</p>
 
                 </div>
-                <div className="current-weather-right">
+                <div className={"current-weather-right current-weather-side" + clazz}>
                     <p className="current-weather-right__wind-speed"><span>Wind speed</span>{currentWeather.wind_kph} km/h</p>
                     <p className="current-weather-right__visibility-km"><span>Visibility</span>{currentWeather.vis_km} km</p>
                 </div>
